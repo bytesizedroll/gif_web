56407bd925f4e8ff81a3473b468bb03f4ae79887
bytesizedroll <damienhobday@gmail.com>
2022-05-03 16:23:42 -0400

    fron page with random gifwq

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
 
-from flask import Flask
+from flask import Flask, render_template
+import json
+import random
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "THE INDEX PAGE"
+    url = "https://api.gfycat.com/v1/gfycats/search?search_text=gif"
+    req = requests.get(url)
+
+    num_gifs = len(req.json()['gfycats'])
+    random_gif = random.randint(0,num_gifs - 1)
+    return render_template("index.html", front_gif=req.json()['gfycats'][random_gif]['max1mbGif'])
 
 @app.route('/search')
 def search():
