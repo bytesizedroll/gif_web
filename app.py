40bb6ff5856bb8988bfbe5f3bc83ffadc0536300
bytesizedroll <damienhobday@gmail.com>
2022-05-03 15:45:44 -0400

    search

--- app.py
+++ app.py
@@ -8,5 +8,9 @@ app = Flask(__name__)
 def index():
     return "THE INDEX PAGE"
 
+@app.route('/search')
+def search():
+    return "THE SEARCH PAGE"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='2424')
