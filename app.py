ca9d0b0eb3f3c2ff8aedbfa49adaa62038d643a7
Damien <damienhobday@gmail.com>
2022-05-15 12:44:38 -0400

    search and trending

--- app.py
+++ app.py
@@ -9,21 +9,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    url = "https://api.gfycat.com/v1/gfycats/search?search_text=gif"
+    url = "https://api.gfycat.com/v1/gfycats/search?search_text=funny%20cat"
     req = requests.get(url)
 
     num_gifs = len(req.json()['gfycats'])
     random_gif = random.randint(0,num_gifs - 1)
-    return render_template("index.html", front_gif=req.json()['gfycats'][random_gif]['max1mbGif'])
+    return render_template("index.html", front_gif=req.json()['gfycats'][random_gif]['max5mbGif'])
 
 @app.route('/search')
 def search():
     return render_template("search.html")
 
-@app.route('/queryGif')
-def queryGif():
-    print('here')
-    return 'Damien'
+
+@app.route('/trending')
+def trending():
+    return render_template("trending.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='2424')
